71b723790879631627971b6e37ef02764d9dd8dd
marco <marcodecrema@libero.it>
2020-10-20 18:14:27 +0200

    home page completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -197,5 +197,60 @@ $data = '[
     </ul>
     @endif
     </div>
+    <footer>
+        <div class="footer_container">
+            <div class="left">
+                <img id="logo-img" src="{{asset('images/logo.png')}}" alt="logo" />
+                <ul>
+                    <li>Ragione Sociale: La Molisana S.P.A.</li>
+                    <li>Sede legale: Contrada Colle Delle Api, 100/A</li>
+                    <li>86100 - Campobasso(CB)</li>
+                    <li>Pec: lamolisana@pec.it</li>
+                    <li>Tel: +39 0874 4981</li>
+                    <li>Fax: +39 0874 629584</li>
+                    <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
+                    <li>commerciale@lamolisana.it</li>
+                    <li>export@lamolisana.it</li>
+                    <li>telefono 380-1292455</li>
+                </ul>
+            </div>
+            <div class="central">
+                <h3>PASTIFICIO</h3>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c'è</li>
+                    <li>Filiera Integrata</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                    <li>Spaghetto Quadrato</li>
+                    <li>Le Persone</li>
+                </ul>
+                <h3>PRODOTTI</h3>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c'è</li>
+                    <li>Filiera Integrata</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                    <li>Spaghetto Quadrato</li>
+                    <li>Le Persone</li>
+                </ul>
+            </div>
+            <div class="right">
+            <h3>COLLEZIONE DA CHEF</h3>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c'è</li>
+                    <li>Filiera Integrata</li>
+                </ul>
+            </div>
+            <div class="mountains">
+                <img id="mountains-img" src="{{asset('images/footer-montagne.jpg')}}" alt="mountains_painter" />
+            </div>
+        </div>
+    </footer>
   </body>
 </html>
